d8bc4dfb9f1e16c2daea3fbb9592a071b96bbbdc
BlyackDaBozz <40516692+BlyackDaBozz@users.noreply.github.com>
2018-06-23 11:29:17 +0200

    Add files via upload

---
+++ main/java/de/blyackdabozz/theextensionsmod/TheExtensionsModConstants.java
@@ -0,0 +1,9 @@
+package de.blyackdabozz.theextensionsmod;
+
+public class TheExtensionsModConstants {
+	
+    public static final String MODID = "theextensionsmod";
+    public static final String NAME = "The Extensions Mod";
+    public static final String VERSION = "Alpha 1.0";
+
+}
